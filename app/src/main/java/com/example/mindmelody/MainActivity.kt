d8d13ab0c38a1719887b1eff1d225821b8fed995
Aditya122221 <adityakumar122221@gmail.com>
2025-08-05 16:06:55 +0530

    Add SplashScreen activity and update AndroidManifest; remove unused MainActivity logic

--- app/src/main/java/com/example/mindmelody/MainActivity.kt
+++ app/src/main/java/com/example/mindmelody/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.mindmelody
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,25 +21,5 @@ class MainActivity : AppCompatActivity() {
                         )
                         insets
                 }
-
-                val sharedPreferences = getSharedPreferences("MindMelodyPref", MODE_PRIVATE)
-                val isTutorialShown = sharedPreferences.getBoolean("tutorialShown", false)
-                val isLogin = sharedPreferences.getBoolean("isLogin", false)
-
-                Handler(Looper.getMainLooper()).postDelayed({
-                        if(isTutorialShown && !isLogin) {
-                                val intent = Intent(this, Login::class.java)
-                                startActivity(intent)
-                                finish()
-                        } else if(isTutorialShown && isLogin) {
-                                val intent = Intent(this, HomePage::class.java)
-                                startActivity(intent)
-                                finish()
-                        } else {
-                                val intent = Intent(this, TutorialPage::class.java)
-                                startActivity(intent)
-                                finish()
-                        }
-                }, 3000)
         }
 }
---
+++ app/src/main/java/com/example/mindmelody/SplashScreen.kt
@@ -0,0 +1,48 @@
+package com.example.mindmelody
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                enableEdgeToEdge()
+                setContentView(R.layout.activity_splash_screen)
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                        val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                        v.setPadding(
+                                systemBars.left,
+                                systemBars.top,
+                                systemBars.right,
+                                systemBars.bottom
+                        )
+                        insets
+                }
+
+                val sharedPreferences = getSharedPreferences("MindMelodyPref", MODE_PRIVATE)
+                val isTutorialShown = sharedPreferences.getBoolean("tutorialShown", false)
+                val isLogin = sharedPreferences.getBoolean("isLogin", false)
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                        if(isTutorialShown && !isLogin) {
+                                val intent = Intent(this, Login::class.java)
+                                startActivity(intent)
+                                finish()
+                        } else if(isTutorialShown && isLogin) {
+                                val intent = Intent(this, HomePage::class.java)
+                                startActivity(intent)
+                                finish()
+                        } else {
+                                val intent = Intent(this, TutorialPage::class.java)
+                                startActivity(intent)
+                                finish()
+                        }
+                }, 3000)
+        }
+}
